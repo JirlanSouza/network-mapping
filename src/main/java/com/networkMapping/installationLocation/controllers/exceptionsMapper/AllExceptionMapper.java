69474f1848f67b7595a3ec1ba03b8c556c2cfb47
Jirlan Souza <jirlansouza08@gmail.com>
2023-02-05 21:13:21 -0400

    feat: implemented the AllExceptionMapper class

---
+++ src/main/java/com/networkMapping/installationLocation/controllers/exceptionsMapper/AllExceptionMapper.java
@@ -0,0 +1,17 @@
+package com.networkMapping.installationLocation.controllers.exceptionsMapper;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AllExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception exception) {
+        var responseError = new HttpResponseError("Internal server error", exception.getMessage());
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(responseError).build();
+    }
+
+}
