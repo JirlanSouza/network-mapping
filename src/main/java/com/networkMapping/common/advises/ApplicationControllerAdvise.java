63aa8e34440ddfe9f7a9750f312f18aa17e7e954
Jirlan Souza <jirlansouza08@gmail.com>
2023-02-26 11:31:58 -0400

    refactor: change the Exception handlers return types

--- src/main/java/com/networkMapping/common/advises/ApplicationControllerAdvise.java
+++ src/main/java/com/networkMapping/common/advises/ApplicationControllerAdvise.java
@@ -4,6 +4,7 @@ import com.networkMapping.networkDevice.domain.exceptions.InvalidNetworkPortSequ
 import com.networkMapping.shared.exceptions.AlreadyExistsEntityException;
 import com.networkMapping.shared.exceptions.NotFoundEntityException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,26 +12,35 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+
 @ControllerAdvice
 public class ApplicationControllerAdvise extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {NotFoundEntityException.class})
-    protected ErrorResponse handleNotFoundError(RuntimeException exception) {
-        return new ErrorResponseException(HttpStatus.NOT_FOUND, exception);
+    protected ProblemDetail handleNotFoundError(RuntimeException exception) {
+        return getDetail(HttpStatus.NOT_FOUND, exception);
     }
 
     @ExceptionHandler(value = {InvalidNetworkPortSequence.class})
-    protected ErrorResponse handleBadRequestError(RuntimeException exception, WebRequest request) {
-        return new ErrorResponseException(HttpStatus.BAD_REQUEST, exception);
+    protected ProblemDetail handleBadRequestError(RuntimeException exception, WebRequest request) {
+        return  getDetail(HttpStatus.BAD_REQUEST, exception);
     }
 
     @ExceptionHandler(value = {AlreadyExistsEntityException.class})
-    protected ErrorResponse handleConflictError(RuntimeException exception) {
-        return new ErrorResponseException(HttpStatus.CONFLICT, exception);
+    protected ProblemDetail handleConflictError(RuntimeException exception) {
+        return getDetail(HttpStatus.CONFLICT, exception);
     }
 
     @ExceptionHandler(value = {Exception.class})
     protected ErrorResponse handleInternalError(Exception exception) {
         return new ErrorResponseException(HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    }
+
+    private ProblemDetail getDetail(HttpStatus status, Exception exception) {
+        var detail = ProblemDetail.forStatus(status);
+        detail.setType(URI.create(exception.getClass().getSimpleName()));
+        detail.setDetail(exception.getMessage());
+        return detail;
     }
 }
