5fff3849f1ec9ae5510dacab5112d10b0b0ef7c2
Jirlan Souza <jirlansouza08@gmail.com>
2023-03-02 19:59:25 -0400

    test: create the SubAreaTest class with sub area tests cases

---
+++ src/test/java/com/networkMapping/installationLocation/domain/SubAreaTest.java
@@ -0,0 +1,33 @@
+package com.networkMapping.installationLocation.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubAreaTest {
+
+    @Test
+    public void shouldCreateNewSubArea() {
+        var parentId = UUID.randomUUID();
+        var subArea = new SubArea("SUB_AREA001", parentId);
+
+        assertNotNull(subArea.getId());
+        assertEquals(subArea.getName(), "SUB_AREA001");
+        assertEquals(subArea.getParentId(), parentId);
+    }
+
+    @Test
+    public void shouldCreateSubAreaWithExistentData() {
+        var id = UUID.randomUUID();
+        var name = "SUB_AREA001";
+        var parentId = UUID.randomUUID();
+        var subArea = new SubArea(id, name, parentId);
+
+        assertEquals(subArea.getId(), id);
+        assertEquals(subArea.getName(), "SUB_AREA001");
+        assertEquals(subArea.getParentId(), parentId);
+    }
+
+}
