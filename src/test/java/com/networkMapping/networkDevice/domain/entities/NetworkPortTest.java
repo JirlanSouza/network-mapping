06b7908b9110a6e122c8f14aa7c23c8b61b6e9b1
Jirlan Souza <jirlansouza08@gmail.com>
2023-03-03 22:09:22 -0400

    test: create the NetworkPortTest class and tests cases

---
+++ src/test/java/com/networkMapping/networkDevice/domain/entities/NetworkPortTest.java
@@ -0,0 +1,45 @@
+package com.networkMapping.networkDevice.domain.entities;
+
+import com.networkMapping.networkDevice.domain.exceptions.InvalidNetworkPortConnection;
+import com.networkMapping.networkDevice.domain.valueObjects.DataSpeedUnit;
+import com.networkMapping.networkDevice.domain.valueObjects.NetworkPortStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NetworkPortTest {
+    @Test
+    public void ShouldCreateNewNetworkPort() {
+        var portNumber = 1;
+        var portType = new NetworkPortType("ethernet", 100, DataSpeedUnit.MBPS);
+        var port = new NetworkPort(portNumber, portType);
+
+        assertNotNull(port.getId());
+        assertEquals(port.getNumber(), portNumber);
+        assertEquals(port.getType(), portType);
+        assertEquals(port.getStatus(), NetworkPortStatus.DISCONNECTED);
+    }
+
+    @Test
+    public void shouldConnectNetworkPort() {
+        var portType = new NetworkPortType("ethernet", 100, DataSpeedUnit.MBPS);
+        var port1 = new NetworkPort(1, portType);
+        var port2 = new NetworkPort(2, portType);
+
+        port1.connect(port2);
+
+        assertEquals(port1.getStatus(), NetworkPortStatus.CONNECTED);
+        assertEquals(port2.getStatus(), NetworkPortStatus.CONNECTED);
+    }
+
+    @Test
+    public void shouldNotConnectNetworkPortAlreadyConnected() {
+        var portType = new NetworkPortType("ethernet", 100, DataSpeedUnit.MBPS);
+        var port1 = new NetworkPort(1, portType);
+        var port2 = new NetworkPort(2, portType);
+        var port3 = new NetworkPort(3, portType);
+        port1.connect(port2);
+
+        assertThrows(InvalidNetworkPortConnection.class, () -> port3.connect(port1));
+    }
+}
